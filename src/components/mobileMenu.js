336587652605889fbc49992b75d6968d24048231
29td <nonhlanhlamndebele9@gmail.com>
2023-01-24 21:15:36 +0200

    Fix linters

--- src/components/mobileMenu.js
+++ src/components/mobileMenu.js
@@ -1,59 +1,59 @@
 /* eslint-disable camelcase */
-import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
-import "../App.css";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import '../App.css';
 
 const MobileMenu = () => {
   const user = useSelector((state) => state.user);
   const [hamburger_class, setHamBurgerClass] = useState(
-    "hamburger-icon unclicked",
+    'hamburger-icon unclicked',
   );
-  const [mobile_menu_class, setMobileMenuClass] = useState("menu hidden");
+  const [mobile_menu_class, setMobileMenuClass] = useState('menu hidden');
   const [isMobileMenuClicked, setIsMobileMenuClicked] = useState(false);
 
   const updateMenu = () => {
     if (!isMobileMenuClicked) {
-      setHamBurgerClass("hamburger-icon clicked");
-      setMobileMenuClass("menu visible");
+      setHamBurgerClass('hamburger-icon clicked');
+      setMobileMenuClass('menu visible');
     } else {
-      setHamBurgerClass("hamburger-icon unclicked");
-      setMobileMenuClass("menu hidden");
+      setHamBurgerClass('hamburger-icon unclicked');
+      setMobileMenuClass('menu hidden');
     }
     setIsMobileMenuClicked(!isMobileMenuClicked);
   };
 
   const [NavbarData, setNavbarData] = useState([
     {
-        name: 'CARS',
-        link: '/',
-      },
-      {
-        name: 'ADD CAR',
-        link: '/add_car',
-      },
-      {
-        name: 'DELETE CAR',
-        link: '/delete_car',
-      },
-      {
-        name: 'MY RESERVATIONS',
-        link: '/reservations',
-      },
-      {
-        name: 'RESERVE',
-        link: '/reserve',
-      },
+      name: 'CARS',
+      link: '/',
+    },
+    {
+      name: 'ADD CAR',
+      link: '/add_car',
+    },
+    {
+      name: 'DELETE CAR',
+      link: '/delete_car',
+    },
+    {
+      name: 'MY RESERVATIONS',
+      link: '/reservations',
+    },
+    {
+      name: 'RESERVE',
+      link: '/reserve',
+    },
   ]);
 
   useEffect(() => {
-    if (user.data.role !== "admin") {
+    if (user.data.role !== 'admin') {
       setNavbarData((prevState) => prevState.slice(0, 3));
     }
   }, []);
 
   return (
-    <div className="all" style={{ width: "100%", height: "100vh" }}>
+    <div className="all" style={{ width: '100%', height: '100vh' }}>
       <nav>
         <button type="button" className="burger-menu" onClick={updateMenu}>
           <div className={hamburger_class} />
